e53974d77d82d3a9ec1cd5fb1316037bb1886cab
pharci kassim <pharci.kassim@groupe-esigelec.org>
2023-02-27 16:37:00 +0100

    creation de population

---
+++ Models/Population.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Population
+{
+	public string Name { get; set; }
+	public int Population { get; set; }
+	public Pays Pays { get; set; }
+
+	public Population(string n, int pop, Pays py) { 
+	Name = n; Population = pop; Pays = py;
+	}
+}
