f3bd1a3f9d9ffd5220aad72fce4e2416d4982aa1
Mathieu CO <mathieu.co@groupe-esigelec.org>
2023-02-27 17:33:46 +0100

    Création de seedData

---
+++ Models/SeedData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using API_Kassim_Co.Data;
+using System;
+using System.Linq;
+namespace API_BDTN.Models;
+public static class SeedData // Ajout d’une nouvelle classe SeedData dans Models pour créer la base et ajouter un film si besoin
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new API_Kassim_CoContext(
+        serviceProvider.GetRequiredService<
+        DbContextOptions<API_Kassim_CoContext>>()))
+        {
+            context.Database.EnsureCreated();
+            // S’il y a déjà des films dans la base
+            if (context.Continent.Any())
+            {
+                return; // On ne fait rien
+            }
+            // Sinon on en ajoute un
+            context.Continent.AddRange(
+            new API_Kassim_Co.Models.Continent
+            {
+                Nom = "France"
+            }
+            );
+            context.SaveChanges();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # API_Kassim_Co
 Projet API Population Pays Continents
+
+L'url pour la base de donnée est "https://localhost:7057;http://localhost:5057"
